10dd32b977ab24287632ee16c4b24fe756868763
Filicipa <filicipa@gmail.com>
2024-12-05 16:36:41 +0200

    init

---
+++ react/build/precache-manifest.5cd8c9934fe1fbb355e206e146de9624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173b0c435eb0ebfca37ac19faf460d62",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "08e16ca6af70a0f8e39b",
+    "url": "/calculator/static/css/2.5861e1ce.chunk.css"
+  },
+  {
+    "revision": "c690212c731bb205ae2b",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "08e16ca6af70a0f8e39b",
+    "url": "/calculator/static/js/2.205ef69f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator/static/js/2.205ef69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c690212c731bb205ae2b",
+    "url": "/calculator/static/js/main.846a7f83.chunk.js"
+  },
+  {
+    "revision": "e765e492bb7ea77de3f8",
+    "url": "/calculator/static/js/runtime-main.0945a1b5.js"
+  }
+]);
